925663834eefd18b84fb05d20269e521894651c6
Ryan Sadler <rrsadler@gmail.com>
2022-06-04 22:46:21 -0700

    update

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -5,6 +5,7 @@ use crate::program::Program;
 use crate::results::*;
 use log::*;
 use std::fmt;
+use rpds::HashTrieMap;
 
 type FunctionSig = Vec<Type>;
 
@@ -16,15 +17,40 @@ pub enum Type {
     //F32,
     //F64,
     Int,
+    Float,
     Bool,
-    Unknown,
+    Unknown(String),
     Void,
     Error,
     Func(FunctionSig),
     Type(String)
 }
 
+impl fmt::Display for Type {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Unknown(s) => write!(f, "T:{}", s),
+            Self::Int => write!(f, "T:Int"),
+            Self::Bool => write!(f, "T:Bool"),
+            Self::Float => write!(f, "T:Float"),
+            Self::Void => write!(f, "T:Void"),
+            Self::Error => write!(f, "T:Error"),
+            Self::Func(s) => write!(f, "T:Func<{:?}>", s),
+            Self::Type(s) => write!(f, "T:Type<{}>", s),
+            _ => unimplemented!()
+        }
+    }
+}
+
+impl Type {
+    pub fn new_unknown(s: String) -> Self {
+        Self::Unknown(s)
+    }
+}
+
 type Environment = crate::compiler::env::EnvLayers<String, IR>;
+
+type SymbolTable = HashTrieMap<String, Type>;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Literal {
@@ -35,7 +61,7 @@ pub enum Literal {
 
 #[derive(Clone, PartialEq)]
 pub struct IR {
-    ty: Option<Type>,
+    ty: Type,
     value: IRValue,
     context: MaybeNodeContext
 }
@@ -44,6 +70,7 @@ impl fmt::Debug for IR {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut out = f.debug_struct("IR");
         out.field("v", &self.value);
+        out.field("ty", &self.ty);
         out.finish()
     }
 }
@@ -61,13 +88,13 @@ pub enum IRValue {
     Block(Vec<IR>),
 
     // A function that is defined externally
-    Extern(String, FunctionSig), // name, args
+    Extern(Vec<IR>), // args
 
     // A function that is defined as part of the program
-    Function(String, FunctionSig, Box<IR>), // name, args, body
+    Function(Box<IR>, Vec<IR>), // body, arg names
 
     // function application
-    Apply(String, Vec<IR>), // function sign, parameters
+    Apply(Box<IR>, Vec<IR>), // function, parameters
 
     Assign(String, Box<IR>),
 
@@ -77,22 +104,24 @@ pub enum IRValue {
 impl crate::compiler::env::LayerValue for IR {}
 
 impl IR {
-    fn new(v: IRValue) -> Self {
-        Self { value: v, ty: None, context: MaybeNodeContext::default() }
+    fn new(v: IRValue, ty: Type, ) -> Self {
+        Self { value: v, ty, context: MaybeNodeContext::default() }
     }
 
-    fn new_with_context(v: IRValue, context: MaybeNodeContext) -> Self {
-        Self { value: v, ty: None, context }
+    fn new_with_context(v: IRValue, ty: Type, context: MaybeNodeContext) -> Self {
+        Self { value: v, ty, context }
     }
 
+    /*
     fn get_type(&self) -> Type {
         match &self.value {
             IRValue::Type(t) => t.clone(),
             IRValue::Extern(_, args) => args.last().unwrap().clone(),
-            IRValue::Function(_, args, _) => args.last().unwrap().clone(),
+            IRValue::Function(args, _) => args.last().unwrap().clone(),
             _ => unimplemented!()
         }
     }
+    */
 }
 
 fn base_env() -> Environment {
@@ -104,23 +133,27 @@ fn op_name(op: &Operator) -> String {
     match op {
         Operator::Plus => "+".into(),
         Operator::Minus => "-".into(),
+        Operator::Exp => "^".into(),
         _ => unimplemented!()
     }
 }
 
+/*
 fn lookup_op(op: &Operator, env: Environment) -> Vec<FunctionSig> {
     let name = op_name(op);
     env.resolve_all(&name).iter().filter_map(|ir| {
         match &ir.value {
-            IRValue::Function(_, sig, _) | IRValue::Extern(_, sig) => {
+            IRValue::Function(sig, _) | IRValue::Extern(_, sig) => {
                 Some(sig.clone())
             }
             _ => None
         }
     }).collect()
 }
+*/
 
 // given types, lookup the function
+/*
 fn lookup_function(name: &String, args: Vec<Type>, env: Environment) -> Vec<FunctionSig> {
     env.resolve_all(&name).iter().filter_map(|ir| {
         match &ir.value {
@@ -145,6 +178,7 @@ fn lookup_function(name: &String, args: Vec<Type>, env: Environment) -> Vec<Func
         }
     }).collect()
 }
+*/
 
 /*
 fn infer_binary_type(op: &Operator, left: &Expr, right: &Expr, env: Environment) -> Type {
@@ -195,9 +229,30 @@ fn infer_type(ast: &Expr, env: Environment) -> Type {
 }
 */
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct TypeEquation {
+    left: Type,
+    right: Type,
+    node: IR,
+}
+
+impl TypeEquation {
+    fn new(left: Type, right: Type, node: IR) -> Self {
+        Self { left, right, node }
+    }
+}
+
+impl fmt::Display for TypeEquation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} :: {}", self.left, self.right)
+    }
+}
+
+
 #[derive(Default)]
 pub struct TypeChecker {
     results: CompileResults,
+    type_equations: Vec<TypeEquation>,
     type_counter: usize
 }
 
@@ -212,18 +267,116 @@ impl TypeChecker {
         format!("t{}", self.next_type_counter())
     }
 
-    fn assign_typenames(&mut self, ir: &IR, env: Environment) {
-        match &ir.value {
-            IRValue::Ident(s) => {
-                if let Some(v) = env.resolve(s) {
-                } else {
-                    //self.results.diagnostics.push(LangError::error("Not found", ir.context).diagnostic());
+    fn generate_equations(&mut self, node: &IR) {
+        match &node.value {
+            IRValue::Type(_) => (),
+            IRValue::Literal(_) => {
+                //self.type_equations.push(TypeEquation::new(node.ty.clone(), node.ty.clone(), node.clone()));
+            }
+            IRValue::Ident(_) => (),
+
+            IRValue::Declare(ident, expr) => {
+                self.generate_equations(&expr);
+                self.type_equations.push(TypeEquation::new(expr.ty.clone(), node.ty.clone(), node.clone()));
+            }
+
+            IRValue::Assign(ident, expr) => {
+                self.generate_equations(&expr);
+                self.type_equations.push(TypeEquation::new(expr.ty.clone(), node.ty.clone(), node.clone()));
+            }
+
+            IRValue::Apply(f, params) => {
+                self.generate_equations(f);
+                for p in params {
+                    self.generate_equations(p);
                 }
             }
-            _ => unimplemented!()
+
+            IRValue::Extern(args) => {
+                // generate type signature for function
+                let mut arg_types = args.iter().map(|v| v.ty.clone()).collect::<Vec<_>>();
+
+                // function has a function type
+                self.type_equations.push(TypeEquation::new(node.ty.clone(), Type::Func(arg_types), node.clone()));
+            }
+
+            IRValue::Function(body, args) => {
+                //for arg in args {
+                    //self.generate_equations(&arg);
+                //}
+                self.generate_equations(body);
+
+                // generate type signature for function
+                let mut arg_types = args.iter().map(|v| v.ty.clone()).collect::<Vec<_>>();
+                // append return type
+                arg_types.push(node.ty.clone());
+
+                // function has a function type
+                self.type_equations.push(TypeEquation::new(node.ty.clone(), Type::Func(arg_types), node.clone()));
+            }
+
+            IRValue::Block(exprs) => {
+                for e in exprs {
+                    self.generate_equations(&e);
+                }
+                let last = exprs.last().unwrap();
+                self.type_equations.push(TypeEquation::new(node.ty.clone(), last.ty.clone(), node.clone()));
+            }
+            //_ => {
+                //debug!("Unimplemented: {:?}", &node);
+                //unimplemented!()
+            //}
         }
     }
 
+    fn assign_typenames(&mut self, ir: &mut IR, mut syms: SymbolTable) -> SymbolTable {
+        match &mut ir.value {
+
+            IRValue::Ident(s) => {
+                // check if the identifier is in the symbol table
+                if let Some(ty) = syms.get(s) {
+                    ir.ty = ty.clone();
+                    syms
+                } else {
+                    self.results.diagnostics.push(LangError::error("Not found".into(), ir.context.clone()).diagnostic());
+                    syms
+                }
+            }
+
+            IRValue::Function(f, args) => {
+                for arg in args {
+                    match &arg.value {
+                        IRValue::Ident(s) => {
+                            syms = syms.insert(s.clone(), arg.ty.clone());
+                        }
+                        _ => unimplemented!()
+                    }
+                }
+                syms = self.assign_typenames(f, syms);
+                syms
+            }
+
+            IRValue::Apply(f, args) => {
+                syms
+            }
+
+            IRValue::Block(_) => {
+                syms
+            }
+            
+            // already typed, and no children
+            IRValue::Literal(_) => syms,
+            _ => {
+                debug!("Unimplemented: {:?}", &ir);
+                unimplemented!()
+            }
+        }
+    }
+
+
+    fn new_unknown_type(&mut self) -> Type {
+        Type::new_unknown(self.get_fresh_typename())
+    }
 
     fn parse_ast(&mut self, node: &ExprNode) -> IR {
         match &node.value {
@@ -234,43 +387,98 @@ impl TypeChecker {
                     body.push(ir);
                 }
 
-                IR::new(IRValue::Function("main".into(), vec![], Box::new(IR::new(IRValue::Block(body)))))
+                IR::new(
+                    IRValue::Function(
+                        Box::new(
+                            IR::new(IRValue::Block(body), self.new_unknown_type())
+                        ),
+                        vec![],  // args
+                    ), self.new_unknown_type())
             }
 
-            Expr::Literal(Tok::FloatLiteral(f)) => IR::new_with_context(IRValue::Literal(Literal::Float(*f)), node.context.clone()),
-            Expr::Literal(Tok::IntLiteral(i)) => IR::new_with_context(IRValue::Literal(Literal::Int(*i)), node.context.clone()),
-            Expr::Literal(Tok::BoolLiteral(b)) => IR::new_with_context(IRValue::Literal(Literal::Bool(*b)), node.context.clone()),
+            Expr::Literal(Tok::FloatLiteral(f)) => IR::new_with_context(
+                IRValue::Literal(Literal::Float(*f)),
+                Type::Float,
+                node.context.clone()),
+            Expr::Literal(Tok::IntLiteral(i)) => IR::new_with_context(
+                IRValue::Literal(Literal::Int(*i)),
+                Type::Int,
+                node.context.clone()),
+            Expr::Literal(Tok::BoolLiteral(b)) => IR::new_with_context(
+                IRValue::Literal(Literal::Bool(*b)),
+                Type::Bool,
+                node.context.clone()),
             Expr::Literal(_) => unimplemented!(),
 
-            Expr::Ident(s) => IR::new_with_context(IRValue::Ident(s.name.clone()), node.context.clone()),
+            Expr::Ident(s) => IR::new_with_context(
+                IRValue::Ident(s.name.clone()),
+                self.new_unknown_type(),
+                node.context.clone()),
 
             Expr::Apply(f, args) => {
-                match &f.value {
-                    Expr::Ident(s) => {
-                        let ir_args = args.iter().map(|v| self.parse_ast(v)).collect::<Vec<_>>(); 
-                        IR::new_with_context(IRValue::Apply(s.name.clone(), ir_args), node.context.clone())
-                    } 
-                    _ => unimplemented!()
-                }
+                let ir_func = self.parse_ast(f);
+                let ir_args = args.iter().map(|v| self.parse_ast(v)).collect::<Vec<_>>(); 
+                IR::new_with_context(
+                    IRValue::Apply(Box::new(ir_func), ir_args),
+                    self.new_unknown_type(),
+                    node.context.clone())
+            }
+
+            Expr::Lambda(f) => {
+                let ir_args = f.params.value.iter().map(|a| {
+                    self.parse_ast(a)
+                }).collect();
+                let body = self.parse_ast(&f.expr);
+                IR::new_with_context(
+                    IRValue::Function(Box::new(body), ir_args),
+                    self.new_unknown_type(),
+                    node.context.clone())
+            }
+
+            Expr::Block(exprs) => {
+                let ir_exprs = exprs.iter().map(|a| {
+                    self.parse_ast(a)
+                }).collect();
+                IR::new_with_context(
+                    IRValue::Block(ir_exprs),
+                    self.new_unknown_type(),
+                    node.context.clone())
             }
 
             Expr::Binary(op, left, right) => {
                 match op {
                     Operator::Assign => {
                         let name = left.try_ident().unwrap().name;
-                        IR::new_with_context(IRValue::Assign(name, Box::new(self.parse_ast(right))), node.context.clone())
+                        IR::new_with_context(
+                            IRValue::Assign(name, Box::new(self.parse_ast(right))),
+                            self.new_unknown_type(),
+                            node.context.clone())
                     }
                     Operator::Declare => {
                         let name = left.try_ident().unwrap().name;
-                        IR::new_with_context(IRValue::Declare(name, Box::new(self.parse_ast(right))), node.context.clone())
+                        IR::new_with_context(
+                            IRValue::Declare(name, Box::new(self.parse_ast(right))),
+                            self.new_unknown_type(),
+                            node.context.clone())
                     }
-                    Operator::Plus | Operator::Minus => {
+                    Operator::Plus | Operator::Minus | Operator::Exp => {
                         let name = op_name(op);
                         let ir_left = self.parse_ast(left);
                         let ir_right = self.parse_ast(right);
-                        IR::new_with_context(IRValue::Apply(name, vec![ir_left, ir_right]), node.context.clone())
+                        let f = IR::new_with_context(
+                            IRValue::Ident(name),
+                            self.new_unknown_type(),
+                            node.context.clone());
+
+                        IR::new_with_context(
+                            IRValue::Apply(Box::new(f), vec![ir_left, ir_right]),
+                            self.new_unknown_type(),
+                            node.context.clone())
                     }
-                    _ => unimplemented!()
+                    _ => {
+                        debug!("Unimplemented: {:?}", &node);
+                        unimplemented!()
+                    }
                 }
                 //let f = op_name(op);
                 //IR::Apply()
@@ -313,12 +521,28 @@ mod tests {
     #[test]
     fn analyze() {
         let mut c = TypeChecker::default();
-        let ir = c.parse_str("1+1");
+        let s = SymbolTable::default();
+        let mut ir = c.parse_str("1+1").unwrap();
+        let s = c.assign_typenames(&mut ir, s);
         debug!("{:?}", ir);
-        let ir = c.parse_str("1-1");
+        let mut ir = c.parse_str("1-1").unwrap();
+        let s = c.assign_typenames(&mut ir, s);
         debug!("{:?}", ir);
-        let ir = c.parse_str("let mut x = 1.");
+        let mut ir = c.parse_str("
+let f = \\x -> { x^2; };
+y = 2
+let mut x = 1.
+let z = f(2)
+").unwrap();
+        let s = c.assign_typenames(&mut ir, s);
+        c.generate_equations(&ir);
         debug!("{:?}", ir);
+        debug!("{:?}", s);
+        c.results.print();
+        for e in c.type_equations {
+            debug!("E: {}", e);
+        }
+
     }
 }
 
